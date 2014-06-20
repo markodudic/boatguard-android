0ff67c50f0bcc54c9291b7533c0231b5bdeee6eb
Marko Dudić <marko.dudic@gmail.com>
2014-06-20 22:33:55 +0000

    maps api

--- src/si/noemus/boatguard/LocationFragment.java
+++ src/si/noemus/boatguard/LocationFragment.java
@@ -6,6 +6,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 public class LocationFragment extends Fragment {
 
 	public LocationFragment() {
@@ -14,6 +19,19 @@ public class LocationFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View rootView = inflater.inflate(R.layout.fragment_location, container, false);
+		
+        // Get a handle to the Map Fragment
+        GoogleMap map = ((MapFragment) getFragmentManager().findFragmentById(R.id.fragment_location)).getMap();
+
+        LatLng sydney = new LatLng(-33.867, 151.206);
+
+        map.setMyLocationEnabled(true);
+        
+        map.addMarker(new MarkerOptions()
+                .title("Sydney")
+                .snippet("The most populous city in Australia.")
+                .position(sydney));
+        
 		return rootView;
 	}
 }
