9283acf72a5c51588630da183d4f7efb7673f605
Marko Dudić <marko.dudic@gmail.com>
2014-12-18 13:23:36 +0000

    customer and obu data

--- src/com/boatguard/boatguard/fragments/MyAccountFragment.java
+++ src/com/boatguard/boatguard/fragments/MyAccountFragment.java
@@ -1,10 +1,14 @@
 package com.boatguard.boatguard.fragments;
 
 import android.app.Fragment;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
@@ -52,16 +56,12 @@ public class MyAccountFragment  extends Fragment {
 
 		final Spinner spinnerBirthyear = (Spinner) v.findViewById(R.id.birthyear);
 		spinnerBirthyear.setSelection(Utils.getIndex(spinnerBirthyear, Settings.customer.getBirth_year()+""));
+		spinnerBirthyear.setOnItemSelectedListener(spinnerSelector);
 
-		final EditText etCountry = (EditText) v.findViewById(R.id.country);
-		etCountry.setText(Settings.customer.getCountry());
-		ImageView ivCountry = (ImageView) v.findViewById(R.id.iv_country);
-		ivCountry.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View vv) {
-				etCountry.setText("");
-			} 
-		});
+		
+		final Spinner spinnerCountry = (Spinner) v.findViewById(R.id.country);
+		spinnerCountry.setSelection(Utils.getIndex(spinnerCountry, Settings.customer.getCountry()));
+		spinnerCountry.setOnItemSelectedListener(spinnerSelector);
 		
 		final EditText etBoatname = (EditText) v.findViewById(R.id.boatname);
 		etBoatname.setText(Settings.customer.getBoat_name());
@@ -73,35 +73,17 @@ public class MyAccountFragment  extends Fragment {
 			} 
 		});
 		
-		final EditText etBoatmanafacturer = (EditText) v.findViewById(R.id.boatmanafacturer);
-		etBoatmanafacturer.setText(Settings.customer.getBoat_manafacturer());
-		ImageView ivBoatmanafacturer = (ImageView) v.findViewById(R.id.iv_boatmanafacturer);
-		ivBoatmanafacturer.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View vv) {
-				etBoatmanafacturer.setText("");
-			} 
-		});
+		final Spinner spinnerManafacturer = (Spinner) v.findViewById(R.id.boatmanafacturer);
+		spinnerManafacturer.setSelection(Utils.getIndex(spinnerManafacturer, Settings.customer.getBoat_manafacturer()));
+		spinnerManafacturer.setOnItemSelectedListener(spinnerSelector);
 		
-		final EditText etBoatmodel = (EditText) v.findViewById(R.id.boatmodel);
-		etBoatmodel.setText(Settings.customer.getBoat_model());
-		ImageView ivBoatmodel = (ImageView) v.findViewById(R.id.iv_boatmodel);
-		ivBoatmodel.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View vv) {
-				etBoatmodel.setText("");
-			} 
-		});
+		final Spinner spinnerModel = (Spinner) v.findViewById(R.id.boatmodel);
+		spinnerModel.setSelection(Utils.getIndex(spinnerModel, Settings.customer.getBoat_model()));
+		spinnerModel.setOnItemSelectedListener(spinnerSelector);
 		
-		final EditText etBoatcountry = (EditText) v.findViewById(R.id.boatcountry);
-		etBoatcountry.setText(Settings.customer.getBoat_country());
-		ImageView ivBoatcountry = (ImageView) v.findViewById(R.id.iv_boatcountry);
-		ivBoatcountry.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View vv) {
-				etBoatcountry.setText("");
-			} 
-		});
+		final Spinner spinnerBoatCountry = (Spinner) v.findViewById(R.id.boatcountry);
+		spinnerBoatCountry.setSelection(Utils.getIndex(spinnerBoatCountry, Settings.customer.getBoat_country()));
+		spinnerBoatCountry.setOnItemSelectedListener(spinnerSelector);
 		
 		EditText etUsername = (EditText) v.findViewById(R.id.username);
 		etUsername.setText(Settings.customer.getUsername());
@@ -123,11 +105,11 @@ public class MyAccountFragment  extends Fragment {
 				Settings.customer.setName(etName.getText().toString());
 				Settings.customer.setSurname(etSurname.getText().toString());
 				Settings.customer.setBirth_year(Integer.parseInt((String)spinnerBirthyear.getSelectedItem()));
-				Settings.customer.setCountry(etCountry.getText().toString());
+				Settings.customer.setCountry((String)spinnerCountry.getSelectedItem());
 				Settings.customer.setBoat_name(etBoatname.getText().toString());
-				Settings.customer.setBoat_manafacturer(etBoatmanafacturer.getText().toString());
-				Settings.customer.setBoat_model(etBoatmodel.getText().toString());
-				Settings.customer.setBoat_country(etBoatcountry.getText().toString());
+				Settings.customer.setBoat_manafacturer((String)spinnerManafacturer.getSelectedItem());
+				Settings.customer.setBoat_model((String)spinnerModel.getSelectedItem());
+				Settings.customer.setBoat_country((String)spinnerBoatCountry.getSelectedItem());
 				Settings.customer.setEmail(etEmail.getText().toString());
 				Settings.setCustomer(getActivity());
 				getActivity().finish();
@@ -136,5 +118,19 @@ public class MyAccountFragment  extends Fragment {
 		
    		return v;
     }
+    
+    private OnItemSelectedListener spinnerSelector = new AdapterView.OnItemSelectedListener() {
+        public void onItemSelected(AdapterView<?> parent, View view, int pos,long id) {
+            ((TextView) parent.getChildAt(0)).setTextColor(getResources().getColor(R.color.text_default_day));
+            ((TextView) parent.getChildAt(0)).setTextSize(getResources().getDimension(R.dimen.spinner_text_size));
+            ((TextView) parent.getChildAt(0)).setBackgroundColor(getResources().getColor(R.color.transparent_color));
+    		Typeface font = Typeface.createFromAsset(getActivity().getAssets(), "fonts/Dosis-Medium.otf");  
+    		((TextView) parent.getChildAt(0)).setTypeface(font); 
+        }
+
+        public void onNothingSelected(AdapterView<?> parent) {
+
+        }
+    };    
      	
 }
