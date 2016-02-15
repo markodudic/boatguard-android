52ebb6899d2737a6ee84df2e89bc48272f0b6335
markodudic <marko.dudic@gmail.com>
2016-02-15 16:46:56 +0100

    add

--- src/com/boatguard/boatguard/fragments/ExtFragment.java
+++ src/com/boatguard/boatguard/fragments/ExtFragment.java
@@ -1,32 +1,68 @@
 package com.boatguard.boatguard.fragments;
 
-import com.boatguard.boatguard.R;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
-import com.boatguard.boatguard.components.TextViewFont;
-import com.boatguard.boatguard.objects.ObuAlarm;
-import com.boatguard.boatguard.utils.Settings;
-import com.boatguard.boatguard.utils.Utils;
+import android.annotation.SuppressLint;
 import android.app.Fragment;
-import android.content.Context;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Switch;
 
-public class AlarmTypeFragment  extends Fragment {
+import com.boatguard.boatguard.R;
+import com.boatguard.boatguard.activities.MainActivity;
+import com.boatguard.boatguard.components.TextViewFont;
+import com.boatguard.boatguard.objects.ObuAlarm;
+import com.boatguard.boatguard.objects.ObuSetting;
+import com.boatguard.boatguard.objects.Setting;
+import com.boatguard.boatguard.utils.Settings;
+import com.boatguard.boatguard.utils.Utils;
+
+public class ExtFragment  extends Fragment {
 	protected LayoutInflater inflater;
-    
+	String type = null;
+	
 	@Override 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         /** Inflating the layout for this fragment **/
-        final View v = inflater.inflate(R.layout.fragment_alarm_type, null);
+        final View v = inflater.inflate(R.layout.fragment_ext, null);
         this.inflater = inflater;
+        
+        type = getArguments().getString("type");    
+        
+		final EditText etName = (EditText) v.findViewById(R.id.extname);
+		etName.setText(getArguments().getString("name"));
+		etName.addTextChangedListener(new TextWatcher(){
+		    public void afterTextChanged(Editable s) {
+		        HashMap<Integer,ObuSetting> obuSettings = Settings.obuSettings;
+		        obuSettings.get(((Setting)Settings.settings.get(type)).getId()).setValue(etName.getText().toString());
+		    }
+		    public void beforeTextChanged(CharSequence s, int start, int count, int after){}
+		    public void onTextChanged(CharSequence s, int start, int before, int count){}
+		}); 
+		ImageView ivName = (ImageView) v.findViewById(R.id.iv_extname);
+		ivName.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View vv) {
+				etName.setText("");
+				etName.requestFocus();
+			} 
+		});
 
+		
         final Switch switchPlaySound = (Switch) v.findViewById(R.id.switch_play_sound);
 		switchPlaySound.setChecked(Utils.getPrefernciesBoolean(getActivity(), Settings.SETTING_PLAY_SOUND, false));
 		switchPlaySound.setOnClickListener(new OnClickListener() {
@@ -57,49 +93,49 @@ public class AlarmTypeFragment  extends Fragment {
 		ObuAlarmAdapter adapter = new ObuAlarmAdapter();
    		ListView lvObuAlarms = (ListView)v.findViewById(R.id.lv_obu_alarms);
    		lvObuAlarms.setAdapter(adapter);
-   		
+		
    		return v;
     }
     
-
-	
     public class ObuAlarmAdapter extends BaseAdapter {
-    	  
+  	  
+		@SuppressLint("NewApi")
 		@Override 
 		public View getView(final int position, View convertView, ViewGroup parent) {
 			
 			View v = inflater.inflate(R.layout.row_obu_alarm, parent, false);
+			final ObuAlarm obuAlarm = (ObuAlarm)getItem(position);
 			
 			TextViewFont tvAlarmMessage = (TextViewFont)v.findViewById(R.id.tv_alarm_message_short);
-			tvAlarmMessage.setText(((ObuAlarm)getItem(position)).getMessage_short().toUpperCase());
+			tvAlarmMessage.setText(obuAlarm.getMessage_short().toUpperCase());
 			tvAlarmMessage.setLetterSpacing(getResources().getInteger(R.integer.letter_spacing_small_set));
 			
 	   		final Switch alarmEnabled = (Switch)v.findViewById(R.id.switch_alarm_enabled);
-			alarmEnabled.setChecked(Settings.obuAlarms.get(position).getActive()==1);
+			alarmEnabled.setChecked(obuAlarm.getActive()==1);
 			alarmEnabled.setOnClickListener(new AdapterView.OnClickListener() {
 	            @Override
 	            public void onClick(View v) {
-	            	((ObuAlarm)getItem(position)).setActive(alarmEnabled.isChecked()?1:0);
+	            	obuAlarm.setActive(alarmEnabled.isChecked()?1:0);
 	            	Settings.setObuAlarms(getActivity());
 	            }
 	        });  
 
 	   		final Switch sendEmail = (Switch)v.findViewById(R.id.switch_send_email);
-			sendEmail.setChecked(Settings.obuAlarms.get(position).getSend_email()==1);
+			sendEmail.setChecked(obuAlarm.getSend_email()==1);
 			sendEmail.setOnClickListener(new AdapterView.OnClickListener() {
 	            @Override
 	            public void onClick(View v) {
-	            	((ObuAlarm)getItem(position)).setSend_email(sendEmail.isChecked()?1:0);
+	            	obuAlarm.setSend_email(sendEmail.isChecked()?1:0);
 	            	Settings.setObuAlarms(getActivity());
 	            }
 	        }); 
 			
 			final Switch alarmFriends = (Switch)v.findViewById(R.id.switch_alarm_friends);
-			alarmFriends.setChecked(Settings.obuAlarms.get(position).getSend_friends()==1);
+			alarmFriends.setChecked(obuAlarm.getSend_friends()==1);
 			alarmFriends.setOnClickListener(new AdapterView.OnClickListener() {
 	            @Override
 	            public void onClick(View v) {
-	            	((ObuAlarm)getItem(position)).setSend_friends(alarmFriends.isChecked()?1:0);
+	            	obuAlarm.setSend_friends(alarmFriends.isChecked()?1:0);
 	            	Settings.setObuAlarms(getActivity());
 	            }
 	        }); 
@@ -108,19 +144,36 @@ public class AlarmTypeFragment  extends Fragment {
 		}
 		@Override
 		public int getCount() {
-			return Settings.obuAlarms.size();
+			return 2;
 		}
 		@Override
 		public Object getItem(int position) {
-			return Settings.obuAlarms.get(position);
+			int id = ((Setting)Settings.settings.get(type)).getId();
+			List<ObuAlarm> obuAlarms = Settings.obuAlarms;
+			for(int i=0; i<obuAlarms.size(); i++) { 
+				ObuAlarm obuAlarm = obuAlarms.get(i);
+				if ((position == 0 && obuAlarm.getId_alarm() == id) || (position == 1 && obuAlarm.getId_alarm() == (id+1))) {
+					return obuAlarm;
+				}
+			}
+			
+			return null;
 		}
 		@Override
 		public long getItemId(int position) {
-			return Settings.obuAlarms.get(position).getId_alarm();
+			int id = ((Setting)Settings.settings.get(type)).getId();
+			List<ObuAlarm> obuAlarms = Settings.obuAlarms;
+			for(int i=0; i<obuAlarms.size(); i++) { 
+				ObuAlarm obuAlarm = obuAlarms.get(i);
+				if ((position == 0 && obuAlarm.getId_alarm() == id) || (position == 1 && obuAlarm.getId_alarm() == (id+1))) {
+					return obuAlarm.getId_alarm();
+				}
+			}
+			
+			return -1;
 		}		
     }	
 	
-	
-	
-	
+
+    
 }
